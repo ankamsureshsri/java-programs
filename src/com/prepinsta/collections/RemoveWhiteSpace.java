4af66205970696e2079399c9a957a126cd51d814
Ankam Suresh <ankamsuresh21@gmail.com>
2022-11-08 15:53:06 +0530

    updated remove white space

--- src/com/prepinsta/collections/RemoveWhiteSpace.java
+++ src/com/prepinsta/collections/RemoveWhiteSpace.java
@@ -4,7 +4,7 @@ public class RemoveWhiteSpace {
 	public static void main(String[] args) {
 		String s = "ankam suresh";
 		String s1 = "";
-		s1 = s.replaceAll(" ", "");
+		s1 = s.replaceAll("\\s", "");
 		System.out.println(s1);
 
 	}
